f46f4c0fc7096e2e4630bff560ef409de5a0e1a0
Wenhan Wang <wenhwang@bu.edu>
2025-02-24 16:09:17 -0500

    fix: remove chat border (#12)
    * remove chat border
    
    * add platform to visualization

--- client/src/components/ChatBot.tsx
+++ client/src/components/ChatBot.tsx
@@ -1,18 +1,11 @@
 import { FC, useEffect, useRef, useState } from "react";
 import { IconButton, Paper, TextField, Box, Typography } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
 import SendIcon from "@mui/icons-material/Send";
-import ChatIcon from "@mui/icons-material/Chat";
 
 export const Chatbot: FC = () => {
-  const [open, setOpen] = useState(false);
   const [message, setMessage] = useState<string>("");
   const [messages, setMessages] = useState<any[]>([]);
   const messagesEndRef = useRef<HTMLDivElement>(null);
-
-  const handleToggle = () => {
-    setOpen(!open);
-  };
 
   const handleSendMessage = async () => {
     if (message.trim() !== "") {
@@ -41,106 +34,72 @@ export const Chatbot: FC = () => {
   }, [messages]);
 
   return (
-    <Box>
-      {!open && (
+    <Box
+      sx={{
+        width: 400,
+        height: 600,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        sx={{
+          flexGrow: 1,
+          p: 1,
+          overflowY: "auto",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {messages.map((msg, index) => (
+          <Box
+            key={index}
+            sx={{
+              maxWidth: "80%",
+              alignSelf: msg.sender === "user" ? "flex-end" : "flex-start",
+              bgcolor: msg.sender === "user" ? "#c00" : "grey.300",
+              color: msg.sender === "user" ? "white" : "black",
+              textAlign: "left",
+              p: 1,
+              borderRadius: 2,
+              mb: 1,
+            }}
+          >
+            <Typography>{msg.text}</Typography>
+          </Box>
+        ))}
+        <div ref={messagesEndRef} />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          py: 1.5,
+          px: 1,
+          borderTop: "1px solid lightgrey",
+          backgroundColor: "grey.100",
+        }}
+      >
+        <TextField
+          fullWidth
+          variant="outlined"
+          size="small"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => e.key === "Enter" && handleSendMessage()}
+          sx={{ backgroundColor: "white" }}
+        />
         <IconButton
-          onClick={handleToggle}
+          onClick={handleSendMessage}
           sx={{
-            backgroundColor: "#c00",
+            ml: 1,
+            bgcolor: "#c00",
             color: "white",
-            p: 2,
-            "&:hover": { backgroundColor: "darkred" },
+            "&:hover": { backgroundColor: "maroon" },
           }}
         >
-          <ChatIcon />
+          <SendIcon />
         </IconButton>
-      )}
-      {open && (
-        <Paper
-          elevation={4}
-          sx={{
-            width: 400,
-            height: 600,
-            display: "flex",
-            flexDirection: "column",
-            border: "2px solid maroon",
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              p: 1,
-              bgcolor: "#c00",
-              color: "white",
-            }}
-          >
-            <Typography variant="h6">AI Chatbot</Typography>
-            <IconButton onClick={handleToggle} sx={{ color: "white" }}>
-              <CloseIcon />
-            </IconButton>
-          </Box>
-          <Box
-            sx={{
-              flexGrow: 1,
-              p: 1,
-              overflowY: "auto",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            {messages.map((msg, index) => (
-              <Box
-                key={index}
-                sx={{
-                  maxWidth: "80%",
-                  alignSelf: msg.sender === "user" ? "flex-end" : "flex-start",
-                  bgcolor: msg.sender === "user" ? "#c00" : "grey.300",
-                  color: msg.sender === "user" ? "white" : "black",
-                  textAlign: "left",
-                  p: 1,
-                  borderRadius: 2,
-                  mb: 1,
-                }}
-              >
-                <Typography>{msg.text}</Typography>
-              </Box>
-            ))}
-            <div ref={messagesEndRef} />
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              py: 1.5,
-              px: 1,
-              borderTop: "1px solid lightgrey",
-              backgroundColor: "grey.100",
-            }}
-          >
-            <TextField
-              fullWidth
-              variant="outlined"
-              size="small"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyPress={(e) => e.key === "Enter" && handleSendMessage()}
-              sx={{ backgroundColor: "white" }}
-            />
-            <IconButton
-              onClick={handleSendMessage}
-              sx={{
-                ml: 1,
-                bgcolor: "#c00",
-                color: "white",
-                "&:hover": { backgroundColor: "maroon" },
-              }}
-            >
-              <SendIcon />
-            </IconButton>
-          </Box>
-        </Paper>
-      )}
+      </Box>
     </Box>
   );
 };
