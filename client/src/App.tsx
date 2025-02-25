a086d1ef382370e9c868635190fd9ca3d68d36ea
wenhwang97 <wenhwang@bu.edu>
2025-02-25 15:22:32 -0500

    add access restriction

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
+import { Box, Typography } from "@mui/material";
 import "./App.css";
 import { Chatbot } from "./components/ChatBot";
 
 function App() {
+  const allowedDomains = /\.bu.edu$/;
+
+  if (!allowedDomains.test(window.location.hostname)) {
+    console.log("Access Denied");
+    return (
+      <Box>
+        <Typography variant="h1">Access Denied</Typography>
+        <Typography variant="body1">
+          This chatbot is only accessible from the Boston University domain.
+        </Typography>
+      </Box>
+    );
+  }
+
   return <Chatbot />;
 }
 
