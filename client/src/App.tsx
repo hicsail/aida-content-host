0e7d5e288885d53d29d0ce510956956a0aa0b1b4
Wenhan Wang <wenhwang@bu.edu>
2025-03-14 16:42:43 -0400

    remove chat embedding from frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 // import { withEmbedding } from "./hoc/withEmbedding";
-import { Chatbot } from "./components/ChatBot";
 import { BrowseDocs } from "./components/BrowseDocs/BrowseDocs";
 import { RootLayout } from "./pages/Root";
 import { TopicModel } from "./pages/TopicModel";
 
 function App() {
-  // const SecuredChatBot = withEmbedding(Chatbot);
   // const SecuredBrowseDocs = withEmbedding(BrowseDocs);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="chat" element={<Chatbot />} />
         <Route path="/" element={<RootLayout />}>
           <Route path="browse" element={<BrowseDocs />} />
           <Route path="topic-model/:topic" element={<TopicModel />} />
